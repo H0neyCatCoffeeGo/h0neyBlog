7afc242dca5d1408b1befd8028955811ea05f636
H0neyCatCoffeeGo <H0neyCatCoffeeGo@proton.me>
2024-03-16 04:40:18 +0500

    style.css hopefully fixed

--- build-site.el
+++ build-site.el
@@ -3,7 +3,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=/home/ok/h0neyBlog/public/style.css >")
+      org-html-head "<link rel=\"stylesheet\" href= ./style.css >")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
