357fefd25ee1bf6da9ffdf76a4659702746c5c3d
H0neyCatCoffeeGo <H0neyCatCoffeeGo@proton.me>
2024-03-16 04:54:03 +0500

    fix

--- build-site.el
+++ build-site.el
@@ -3,7 +3,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\ "./style.css" >")
+      org-html-head "<link rel=\"stylesheet\" href=\ "https://raw.githubusercontent.com/H0neyCatCoffeeGo/h0neyBlog/main/public/style.css" >")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
